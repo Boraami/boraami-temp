45322e0aa870dfcb8a591d767160cbb732bf8be9
fabcodingzest <fabrizvi786@gmail.com>
2023-04-21 10:45:30 +0530

    coded: Landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,51 @@
-import { useState } from 'react'
-import boraamiLogo from './assets/boraami-primary-horizontal-logo.svg'
-import './App.css'
+import boraamiLogo from "./assets/boraami-primary-horizontal-logo.svg";
+import "./App.css";
+import { FaInstagramSquare, FaTwitterSquare } from "react-icons/fa";
+import TiktokIcon from "./assets/TiktokIcon";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <img src={boraamiLogo} className="logo"></img>
-      <h2> Coming soon...</h2>
-    </div>
-  )
+type IconType = (props: LinkProps) => JSX.Element;
+interface LinkProps {
+  link: string;
+  icon: JSX.Element;
 }
 
-export default App
+const SocialLink = ({ link, icon }: LinkProps) => {
+  return (
+    <a target="_blank" className="social-icon" href={link}>
+      {icon}
+    </a>
+  );
+};
+
+const App = () => {
+  return (
+    <div className="App">
+      <header>
+        <h1>
+          <img src={boraamiLogo} className="logo" alt="Boraami Logo" />
+        </h1>
+        <h2> Coming soon</h2>
+      </header>
+      <main>
+        <h3>
+          A safe and inclusive social media app for <span className="bold">ARMY</span> to stay connected, and support all the <span className="bold">BTS</span> members
+        </h3>
+        <div className="socials">
+          <span className="star">&#10022;</span>
+          <h4>Let’s stay in touch and follow us on:</h4>
+          <div className="social-media-icons">
+            <SocialLink link="https://twitter.com/boraami_app" icon={<FaTwitterSquare />} />
+            <a target="_blank" className="social-icon" href="https://www.instagram.com/boraami_app/">
+              <FaInstagramSquare />
+            </a>
+            <a target="_blank" className="social-icon tiktok" href="https://www.tiktok.com/@boraami_app">
+              <TiktokIcon />
+            </a>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default App;
