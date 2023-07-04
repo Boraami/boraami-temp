f094d8b8a976d5406b0e9974b47407cbea4a8cc1
fabcodingzest <fabrizvi786@gmail.com>
2023-07-05 00:52:38 +0530

    created FAQ file

---
+++ src/constants/Faqs.ts
@@ -0,0 +1,56 @@
+export interface FAQType {
+  question: string;
+  answer: string;
+}
+
+export const FAQs: FAQType[] = [
+  {
+    question: `What is the purpose of Boraami?`,
+    answer: `To provide a safe and inclusive space for ARMY to stay connected, lead with kindness and seamlessly support all the BTS members.`,
+  },
+  {
+    question: `Who can join the app?`,
+    answer: `OT7 ARMY only. We want to keep the app environment positive and will not encourage or tolerate any negativity against any member or ARMY`,
+  },
+  {
+    question: `Will anyone be able to create an account?`,
+    answer: `We’ve come up with a way for only ARMY to create accounts and will do our best to keep this space safe. Please be patient with us as we figure it out as we go.`,
+  },
+  {
+    question: `Will BTS be a part of Boraami?`,
+    answer: `Since this is an unofficial fan app, BTS members will not be a part of it, but if they would like to join in the future, we’re definitely laying out that purple carpet 🥰`,
+  },
+  {
+    question: `What features will Boraami have?`,
+    answer: `We’re still working on understanding the technical feasibility for some of the features, so we don’t want to preemptively make any announcements, but it will be a social media app that is focussed on streaming and voting so we can support all the BTS members`,
+  },
+  {
+    question: `Will there be any cost associated with using the app?`,
+    answer: `No cost! Just your love for BTS 😊 It’s completely free!`,
+  },
+  {
+    question: `Will this be available in the App Store or Play Store?`,
+    answer: `Since this will be the first version of Boraami, we are going to have a web app to begin with. You will be able to bookmark the website on your phone and use it. We do plan on having it be a mobile app in a future version.`,
+  },
+  {
+    question: `How will the app stay up-to-date with the latest BTS 
+news and information?`,
+    answer: `Once we are closer to releasing Boraami, we will be looking for several moderators and admins to keep up to up to date with all the releases and voting information. We will of course rely on you all to let us know if we miss anything once in a while.`,
+  },
+  {
+    question: `Will it be available in other languages?`,
+    answer: `To start with, we will only have English and Korean because we don’t want to put too much pressure on our development team, but we will try to include Spanish in the first version if possible. We will continue to progressively add more languages in the future.`,
+  },
+  {
+    question: `Will it be available in every country?`,
+    answer: `Yes! Since it will be a web app to start with, it will work anywhere that internet is available. We’ll do our due diligence to make sure that the app will be available in the app store/play store in the future too. We would like to have ARMY from all around the world be a part of this community.`,
+  },
+  {
+    question: `Who are the people behind the app?`,
+    answer: `The accounts that Boraami follows on all social media platforms are the people behind the app. We’re from all over the world and have come together for the love of BTS & ARMY 🫶`,
+  },
+  {
+    question: `When will the Boraami app be available?`,
+    answer: `Our team is working very hard and we’re hoping to make it available by the mid-end of 2024. Your support means the world to us, so thank you for being here ARMY. Borahae 💜`,
+  },
+];
