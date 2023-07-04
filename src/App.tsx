9bb39497bbcc98a76600dd79526bd78be038d5e1
fabcodingzest <fabrizvi786@gmail.com>
2023-07-05 00:53:15 +0530

    added FAQList to app

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import boraamiLogo from "./assets/boraami-primary-horizontal-logo.svg";
 import "./App.css";
 import { FaInstagramSquare, FaTwitterSquare } from "react-icons/fa";
 import TiktokIcon from "./assets/TiktokIcon";
+import FAQList from "./components/FAQList";
 
 interface LinkProps {
   link: string;
   icon: JSX.Element;
-  linkType: string;
 }
 
-const SocialLink = ({ link, icon, linkType }: LinkProps) => {
+const SocialLink = ({ link, icon }: LinkProps) => {
   return (
     <a target="_blank" className="social-icon" href={link}>
       {icon}
@@ -19,27 +19,32 @@ const SocialLink = ({ link, icon, linkType }: LinkProps) => {
 
 const App = () => {
   return (
-    <div className="App">
-      <header>
-        <h1>
-          <img src={boraamiLogo} className="logo" alt="Boraami Logo" />
-        </h1>
-        <h2> Coming soon</h2>
-      </header>
-      <main>
-        <h3>
-          A safe and inclusive social media app for <span className="bold">ARMY</span> to stay connected, and support all the <span className="bold">BTS</span> members
-        </h3>
-        <div className="socials">
-          <span className="star">&#10022;</span>
-          <h4>Let’s stay in touch and follow us on:</h4>
-          <div className="social-media-icons">
-            <SocialLink link="https://twitter.com/boraami_app" linkType="twitter" icon={<FaTwitterSquare />} />
-            <SocialLink link="https://www.instagram.com/boraami_app/" linkType="instagram" icon={<FaInstagramSquare />} />
-            <SocialLink link="https://www.tiktok.com/@boraami_app" linkType="tiktok" icon={<TiktokIcon />} />
+    <div className="app">
+      <div className="hero">
+        <header>
+          <h1>
+            <img src={boraamiLogo} className="logo" alt="Boraami Logo" />
+          </h1>
+          <h2> Coming soon</h2>
+        </header>
+        <main>
+          <h3>
+            A safe and inclusive social media app for <span className="bold">ARMY</span> to stay connected, and support all the <span className="bold">BTS</span> members
+          </h3>
+          <div className="socials">
+            <span className="star">&#10022;</span>
+            <h4>Let’s stay in touch and follow us on:</h4>
+            <div className="social-media-icons">
+              <SocialLink link="https://twitter.com/boraami_app" icon={<FaTwitterSquare />} />
+              <SocialLink link="https://www.instagram.com/boraami_app/" icon={<FaInstagramSquare />} />
+              <SocialLink link="https://www.tiktok.com/@boraami_app" icon={<TiktokIcon />} />
+            </div>
           </div>
-        </div>
-      </main>
+        </main>
+      </div>
+      <section className="faqs">
+        <FAQList />
+      </section>
     </div>
   );
 };
